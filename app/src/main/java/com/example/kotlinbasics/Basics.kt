07b5827a034592f913bc2e26ac66c54c6c2dc938
Kevin <kevin.castro.chavez0@gmail.com>
2022-09-21 12:11:33 -0600

    learned about difference between var and val

--- app/src/main/java/com/example/kotlinbasics/Basics.kt
+++ app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -1,2 +1,16 @@
 package com.example.kotlinbasics
 
+// Special function that runs the application
+fun main(){
+//    var myName = "Kevin Castro"
+//    myName = "Jared Castro"
+
+//    Val cannot be overwritten
+    val myName = "Kevin Castro"
+    print("Hello, " + myName)
+}
+
+// Normal function
+//fun hello(){
+//
+//}
