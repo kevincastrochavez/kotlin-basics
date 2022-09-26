c7c517ab76116e032b41aa6a486f3c8ed5fc16a9
Kevin <kevin.castro.chavez0@gmail.com>
2022-09-26 09:41:10 -0600

    learned about while and do loops

--- app/src/main/java/com/example/kotlinbasics/Basics.kt
+++ app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -53,36 +53,53 @@ fun main(){
 //    if (isRainy)
 //        print("It's rainy")
 
-    var season = 3
-    when(season){
-        1 -> println("Spring")
-        2 -> println("Summer")
-        3 -> {
-            println("Fall")
-            println("Autumn")
-        }
-        4 -> println("Winter")
-        else -> println("Invalid season")
+
+//    WHEN EXPRESSIONS
+//    var season = 3
+//    when(season){
+//        1 -> println("Spring")
+//        2 -> println("Summer")
+//        3 -> {
+//            println("Fall")
+//            println("Autumn")
+//        }
+//        4 -> println("Winter")
+//        else -> println("Invalid season")
+//    }
+//
+//    var month = 3
+//    when(month) {
+//        in 3..5 -> println("Spring")
+//        in 6..8 -> println("Summer")
+//        in 9..11 -> println("Fall")
+//        12, 1, 2 -> println("Winter")
+//        in 12 downTo 2 -> println("Winter")
+//        else -> println("Invalid season")
+//    }
+//
+//    var x: Any = 13.37
+//    when(x) {
+//        is Int -> println("$x is an Int")
+////        is Double -> println("$x is a Double")
+//        is String -> println("$x is a String")
+//        !is String -> println("$x is not a String")
+//        else -> println("None of the above")
+//    }
+
+
+//    WHILE LOOPS
+
+    var x = 0
+    while (x <= 10) {
+        println(x)
+        x++
     }
 
-    var month = 3
-    when(month) {
-        in 3..5 -> println("Spring")
-        in 6..8 -> println("Summer")
-        in 9..11 -> println("Fall")
-        12, 1, 2 -> println("Winter")
-        in 12 downTo 2 -> println("Winter")
-        else -> println("Invalid season")
-    }
-
-    var x: Any = 13.37
-    when(x) {
-        is Int -> println("$x is an Int")
-//        is Double -> println("$x is a Double")
-        is String -> println("$x is a String")
-        !is String -> println("$x is not a String")
-        else -> println("None of the above")
-    }
+    var y = 0
+    do {
+        println(y)
+        y++
+    } while (y <= 10)
 }
 
 // Normal function
