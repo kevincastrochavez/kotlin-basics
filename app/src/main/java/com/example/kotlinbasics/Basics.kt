a9427010fa30b014bb9c9a6d793e529167e4bb2d
Kevin <kevin.castro.chavez0@gmail.com>
2022-09-26 09:24:47 -0600

    learned about the when expressions using the in, is and downTo operators

--- app/src/main/java/com/example/kotlinbasics/Basics.kt
+++ app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -9,49 +9,80 @@ fun main(){
 //    val myName = "Kevin Castro"
 //    print("Hello, " + myName)
 
-    var myAge: Int = 25
-    val myFloat: Float = 34.45F
-    val myInt: Double = 3.1416323534545
-    var isSunny: Boolean = true
-    isSunny = false
+//    var myAge: Int = 25
+//    val myFloat: Float = 34.45F
+//    val myInt: Double = 3.1416323534545
+//    var isSunny: Boolean = true
+//    isSunny = false
 
-    val letterChar = "A"
-    val digitChar = "1"
+//    val letterChar = "A"
+//    val digitChar = "1"
+//
+//    val myStr = "Hello World"
+//    var firstCharStr = myStr[0]
+////    val myLength = myStr.length
+////    print("First character $firstCharStr and the length is ${myStr.length}")
+//
+//    var result = (5.0 / 3)
+////    var result = (5.0 / 3).toInt()
+////    print(result)
+//
+//    val isEqual = 5 == 5
+////    print("isEqual is $isEqual")
 
-    val myStr = "Hello World"
-    var firstCharStr = myStr[0]
-//    val myLength = myStr.length
-//    print("First character $firstCharStr and the length is ${myStr.length}")
-
-    var result = (5.0 / 3)
-//    var result = (5.0 / 3).toInt()
-//    print(result)
-
-    val isEqual = 5 == 5
-//    print("isEqual is $isEqual")
-
-    var myNum = 5
-    myNum += 3
-    myNum++
-    --myNum
+//    var myNum = 5
+//    myNum += 3
+//    myNum++
+//    --myNum
 
 //    print(myNum)
 
-    var heightPerson1 = 170
-    var heightPerson2 = 189
+//    var heightPerson1 = 170
+//    var heightPerson2 = 189
+//
+//    if (heightPerson1 > heightPerson2) {
+//        print("This is crazy")
+//    } else if (heightPerson1 == heightPerson2) {
+//       print("Equals case")
+//    } else {
+//        print("This is true")
+//    }
+//
+////    Second way of writing if statements
+//    var isRainy = true
+//    if (isRainy)
+//        print("It's rainy")
 
-    if (heightPerson1 > heightPerson2) {
-        print("This is crazy")
-    } else if (heightPerson1 == heightPerson2) {
-       print("Equals case")
-    } else {
-        print("This is true")
+    var season = 3
+    when(season){
+        1 -> println("Spring")
+        2 -> println("Summer")
+        3 -> {
+            println("Fall")
+            println("Autumn")
+        }
+        4 -> println("Winter")
+        else -> println("Invalid season")
     }
 
-//    Second way of writing if statements
-    var isRainy = true
-    if (isRainy)
-        print("It's rainy")
+    var month = 3
+    when(month) {
+        in 3..5 -> println("Spring")
+        in 6..8 -> println("Summer")
+        in 9..11 -> println("Fall")
+        12, 1, 2 -> println("Winter")
+        in 12 downTo 2 -> println("Winter")
+        else -> println("Invalid season")
+    }
+
+    var x: Any = 13.37
+    when(x) {
+        is Int -> println("$x is an Int")
+//        is Double -> println("$x is a Double")
+        is String -> println("$x is a String")
+        !is String -> println("$x is not a String")
+        else -> println("None of the above")
+    }
 }
 
 // Normal function
